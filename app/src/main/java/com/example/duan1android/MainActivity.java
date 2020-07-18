51ff0640d026ad4653a469e780bad079fbb7ddaa
oppabgnamdz <oppabgnamdz@gmail.com>
2020-07-18 07:49:57 +0700

    thanhnam

--- app/src/main/java/com/example/duan1android/MainActivity.java
+++ app/src/main/java/com/example/duan1android/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab    = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
