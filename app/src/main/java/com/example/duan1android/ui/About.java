64664c369c080173fd216cf7baa453c9832429f7
man <oppabgnamdz@gmail.com>
2020-08-11 07:50:06 +0700

    add about

--- app/src/main/java/com/example/duan1android/ui/About.java
+++ app/src/main/java/com/example/duan1android/ui/About.java
@@ -18,6 +18,8 @@ public class About extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        return view;
     }
 }
